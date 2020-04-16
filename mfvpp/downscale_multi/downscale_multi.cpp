36d05ef4a1734066113af86888b15156753939fa
lli108 <liang.a.li@intel.com>
2020-04-16 09:08:30 +0800

    add downscale_multi project

---
+++ mfvpp/downscale_multi/downscale_multi.cpp
@@ -0,0 +1,348 @@
+
+#include "cm_rt.h"
+#include <assert.h>
+#include <iostream>
+#include <limits>
+#include <stdio.h>
+#include <fstream>
+
+using namespace std;
+
+#define QUEUE_NUM  1
+#define KERNEL_NUM 1
+
+#define DS_THREAD_WIDTH 16
+#define DS_THREAD_HEIGHT 16
+
+char* gKernelNameList[2] = { "downscale16", "downscale32" };
+
+struct ImgData {
+    int w;
+    int h;
+    int size;
+    char* buf;
+};
+
+struct KernelContext
+{
+    CmKernel* kernel;
+    CmSurface2D* srcSurf;
+    CmSurface2D* dstSurf;
+    SurfaceIndex* srcIdx;
+    SurfaceIndex* dstIdx;
+    CmSampler* sampler;
+    SamplerIndex* samplerIdx;
+    CmEvent* event;
+};
+
+struct QueueContext
+{
+    CmQueue* queue;
+    CmTask* task;
+    CmThreadSpace* ts;
+    CmThreadGroupSpace* tgs;
+    KernelContext kctx[KERNEL_NUM];
+};
+
+int runNum = 100;
+int srcW, srcH, dstW, dstH;
+ImgData img = {};
+int kIndex = 0;
+int cmRet = 0;
+CmDevice* pCmDev = NULL;
+CmProgram* pProgram = NULL;
+void* pCommonISACode = NULL;
+QueueContext queueCtx[QUEUE_NUM] = {};
+
+int dstSizeY = dstW * dstH;
+int dstSize = dstW * dstH * 3 / 2;
+unsigned char* pSrcMem;
+unsigned char* pDstMem;
+
+void readNV12(char*filename, ImgData &img)
+{
+    ifstream infile(filename, ios::binary);
+    img.buf = new char[img.size];
+    memset(img.buf, 0, img.size);
+    infile.read(img.buf, img.size);
+    infile.close();
+}
+
+void dumpNV12(char* filename, char* buf, int size)
+{
+    ofstream of(filename, ios::binary);
+    of.write(buf, size);
+    of.close();
+}
+
+int cmdOpt(int argc, char** argv)
+{
+    if (argc == 1) {
+        srcW = 1920;
+        srcH = 1080;
+        dstW = 300;
+        dstH = 300;
+        img.w = srcW;
+        img.h = srcH;
+        img.size = srcW * srcH * 3 / 2;
+        readNV12("test.nv12", img);
+    } else if (argc == 6 || argc == 7) {
+        srcW = atoi(argv[1]);
+        srcH = atoi(argv[2]);
+        dstW = atoi(argv[3]);
+        dstH = atoi(argv[4]);
+        img.w = srcW;
+        img.h = srcH;
+        img.size = srcW * srcH * 3 / 2;
+        readNV12(argv[5], img);
+        kIndex = (argc == 7) ? ((atoi(argv[6]) == 0) ? 0 : 1) : 0;
+    } else {
+        printf("ERROR: invalid cmd line!\n");
+        return -1;
+    }
+    return 0;
+}
+
+int initKernel(KernelContext* kctx, CmTask* task)
+{
+    cmRet = pCmDev->CreateKernel(pProgram, gKernelNameList[kIndex], kctx->kernel);
+    if (cmRet != CM_SUCCESS) {
+        printf("ERROR: CM CreateKernel error\n");
+        return -1;
+    }
+
+    // 3D Sampler
+    CM_SAMPLER_STATE  sampleState = {};
+    sampleState.magFilterType = CM_TEXTURE_FILTER_TYPE_LINEAR;
+    sampleState.minFilterType = CM_TEXTURE_FILTER_TYPE_LINEAR;
+    sampleState.addressU = CM_TEXTURE_ADDRESS_CLAMP;
+    sampleState.addressV = CM_TEXTURE_ADDRESS_CLAMP;
+    sampleState.addressW = CM_TEXTURE_ADDRESS_CLAMP;
+    cmRet = pCmDev->CreateSampler(sampleState, kctx->sampler);
+    if (cmRet != CM_SUCCESS) {
+        printf("ERROR: CM CreateSampler error\n");
+        return -1;
+    }
+    kctx->sampler->GetIndex(kctx->samplerIdx);
+
+    // Source surface
+    unsigned int pitch, physicalSize;
+    if (((unsigned int)pSrcMem & 0xfff) == 0) {
+        pCmDev->GetSurface2DInfo(srcW, srcH, CM_SURFACE_FORMAT_NV12, pitch, physicalSize);
+        printf("INFO: CmSurface2D w = %d, h = %d, pitch = %d, size = %d\n", srcW, srcH, pitch, physicalSize);
+    }
+    cmRet = pCmDev->CreateSurface2D(srcW, srcH, CM_SURFACE_FORMAT_NV12, kctx->srcSurf);
+    if (cmRet != CM_SUCCESS) {
+        printf("ERROR: CM CreateSurface2D error\n");
+        return -1;
+    }
+    cmRet = kctx->srcSurf->WriteSurface(pSrcMem, NULL);
+    if (cmRet != CM_SUCCESS) {
+        printf("ERROR: CM WriteSurface error\n");
+        return -1;
+    }
+    cmRet = pCmDev->CreateSamplerSurface2D(kctx->srcSurf, kctx->srcIdx);
+    if (cmRet != CM_SUCCESS) {
+        printf("CM CreateSamplerUPSurface error");
+        return -1;
+    }
+
+    // Destination surface
+    cmRet = pCmDev->CreateSurface2D(dstW, dstH, CM_SURFACE_FORMAT_NV12, kctx->dstSurf);
+    if (cmRet != CM_SUCCESS) {
+        printf("ERROR: CM CreateSurface2D error\n");
+        return -1;
+    }
+    kctx->dstSurf->GetIndex(kctx->dstIdx);
+
+    CmKernel* kernel = kctx->kernel;
+    int threadWidth = (dstW + DS_THREAD_WIDTH - 1) / DS_THREAD_WIDTH;
+    int threadHeight = (dstH + DS_THREAD_HEIGHT - 1) / DS_THREAD_HEIGHT;
+    kernel->SetThreadCount(threadWidth * threadHeight);
+    printf("INFO: HW thread_w = %d, thread_h = %d, total = %d\n", 
+        threadWidth, threadHeight, threadWidth * threadHeight);
+
+    kernel->SetKernelArg(0, sizeof(SurfaceIndex), kctx->srcIdx);
+    kernel->SetKernelArg(1, sizeof(SamplerIndex), kctx->samplerIdx);
+    kernel->SetKernelArg(2, sizeof(SurfaceIndex), kctx->dstIdx);
+    kernel->SetKernelArg(3, sizeof(unsigned short), &dstW);
+    kernel->SetKernelArg(4, sizeof(unsigned short), &dstH);
+
+    cmRet = task->AddKernel(kernel);
+    if (cmRet != CM_SUCCESS) {
+        printf("ERROR: CmDevice AddKernel error\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+int initQueue()
+{
+    int cmRet = 0;
+    for (int i=0; i<QUEUE_NUM; i++)
+    {
+        cmRet = pCmDev->CreateQueue(queueCtx[i].queue);
+        if (cmRet != CM_SUCCESS) {
+            printf("ERROR: CM CreateQueue error\n");
+            return -1;
+        }
+
+        cmRet = pCmDev->CreateTask(queueCtx[i].task);
+        if (cmRet != CM_SUCCESS) {
+            printf("ERROR: CmDevice CreateTask error\n");
+            return -1;
+        }
+
+        int threadWidth = (dstW + DS_THREAD_WIDTH - 1) / DS_THREAD_WIDTH;
+        int threadHeight = (dstH + DS_THREAD_HEIGHT - 1) / DS_THREAD_HEIGHT;
+        cmRet = pCmDev->CreateThreadSpace(threadWidth, threadHeight, queueCtx[i].ts);
+        if (cmRet != CM_SUCCESS) {
+            printf("ERROR: CM CreateThreadSpace error\n");
+            return -1;
+        }
+
+        for (int j=0; j<KERNEL_NUM; j++)
+        {
+            if (initKernel(&queueCtx[i].kctx[j], queueCtx[i].task)) {
+                return -1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+int initCM(int& result)
+{
+    UINT version = 0;
+    result = ::CreateCmDevice(pCmDev, version);
+    if (result != CM_SUCCESS) {
+        printf("ERROR: CmDevice creation error");
+        return -1;
+    }
+    if (version < CM_1_0) {
+        printf("ERROR: The runtime API version is later than runtime DLL version");
+        return -1;
+    }
+
+    FILE* pFileISA = nullptr;
+    fopen_s(&pFileISA, "downscale_genx.isa", "rb");
+    if (pFileISA == NULL) {
+        printf("ERROR: failed to open downscale_genx.isa\n");
+        return -1;
+    }
+    fseek(pFileISA, 0, SEEK_END);
+    int codeSize = ftell(pFileISA);
+    rewind(pFileISA);
+    if (codeSize == 0) {
+        printf("ERROR: invalid ISA file\n");
+        return -1;
+    }
+    pCommonISACode = (BYTE*)malloc(codeSize);
+    if (!pCommonISACode) {
+        printf("ERROR: failed to allocate memory for ISA code, size = %d\n", codeSize);
+        return -1;
+    }
+    if (fread(pCommonISACode, 1, codeSize, pFileISA) != codeSize) {
+        printf("ERROR: failed to read ISA file\n");
+        return -1;
+    }
+    fclose(pFileISA);
+
+    result = pCmDev->LoadProgram(pCommonISACode, codeSize, pProgram);
+    if (result != CM_SUCCESS) {
+        printf("ERROR: CM LoadProgram error\n");
+        return -1;
+    }
+
+    if (initQueue()) {
+        return -1;
+    }
+
+    return 0;
+}
+
+int initBuf()
+{
+    pDstMem = (unsigned char*)malloc(dstSize);
+    if (NULL == pDstMem) {
+        printf("ERROR: pDstMem alloc failed\n");
+        return -1;
+    }
+
+    pSrcMem = (unsigned char*)_aligned_malloc(img.size, 0x1000);
+    if (pSrcMem) {
+        memset(pSrcMem, 0, img.size);
+        memcpy_s(pSrcMem, img.size, img.buf, img.size);
+    }
+    else {
+        printf("ERROR: alloc pSrcMem failed\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    if (cmdOpt(argc, argv)) {
+        return -1;
+    }
+
+    if (initBuf()) {
+        return -1;
+    }
+
+    if (initCM(cmRet)) {
+        return -1;
+    }
+
+    double totalTime = 0.0;
+    
+    for (size_t i = 0; i < runNum; i++) {
+
+        for (int j=0; j<QUEUE_NUM; j++)
+        {
+            CmQueue* pCmQueue = queueCtx[j].queue;
+            CmTask* task = queueCtx[j].task;
+            CmThreadSpace* ts = queueCtx[j].ts;
+            CmEvent* e = queueCtx[j].kctx[0].event;
+            CmSurface2D* pDstSurface = queueCtx[j].kctx[0].dstSurf;
+
+            cmRet = pCmQueue->Enqueue(task, e, ts);
+            if (cmRet != CM_SUCCESS) {
+                printf("ERROR: CmDevice enqueue error\n");
+                return -1;
+            }
+
+            cmRet = pDstSurface->ReadSurface(pDstMem, e);
+            if (cmRet != CM_SUCCESS) {
+                printf("ERROR: CM ReadSurface error\n");
+                return -1;
+            }
+
+            UINT64 executionTime = 0;
+            cmRet = e->GetExecutionTime(executionTime);
+            if (cmRet != CM_SUCCESS) {
+                printf("CM GetExecutionTime error");
+                return -1;
+            }
+
+            totalTime += executionTime;
+        }
+
+        static int dumped = 0;
+        if (!dumped) {
+            dumped = 1;
+            dumpNV12("out.nv12", (char*)pDstMem, dstSize);
+        }
+    }
+
+    printf("INFO: Average execution time: %f us; run_times = %d\n", totalTime/runNum/1000.0, runNum);
+
+
+
+    printf("done!\n");
+    return 0;
+}
---
+++ mfvpp/downscale_multi/downscale_multi_genx.cpp
@@ -0,0 +1,129 @@
+/**             
+***
+*** Copyright  (C) 1985-2011 Intel Corporation. All rights reserved.
+***
+*** The information and source code contained herein is the exclusive
+*** property of Intel Corporation. and may not be disclosed, examined
+*** or reproduced in whole or in part without explicit written authorization
+*** from the company.
+***
+*** ----------------------------------------------------------------------------
+**/ 
+#include <cm/cm.h>
+
+#define DS_THREAD_WIDTH 16
+#define DS_THREAD_HEIGHT 16
+
+extern "C" _GENX_MAIN_ void
+downscale32(SurfaceIndex src_idx,
+          SamplerIndex sampler_idx,
+          SurfaceIndex dst_idx,
+          ushort image_width, 
+          ushort image_height)
+{ 
+    ushort idx = get_thread_origin_x();
+    ushort idy = get_thread_origin_y();
+    ushort x = get_thread_origin_x() * DS_THREAD_WIDTH;
+    ushort y = get_thread_origin_y() * DS_THREAD_HEIGHT; 
+
+    // calc interval
+    float x_interval = 1.0f / (image_width-1);
+    float y_interval = 1.0f / (image_height-1);
+
+    float x0 = x_interval * x;
+    float y0 = y_interval * y;
+
+    matrix<ushort, 3, 32> sampler_out;
+    matrix<uchar, DS_THREAD_HEIGHT, DS_THREAD_WIDTH> out_y;
+    matrix<uchar, DS_THREAD_HEIGHT, DS_THREAD_WIDTH> tmp_u;
+    matrix<uchar, DS_THREAD_HEIGHT, DS_THREAD_WIDTH> tmp_v;
+    matrix<uchar, DS_THREAD_HEIGHT / 2, DS_THREAD_WIDTH> out_uv;
+
+#pragma unroll
+    for (int i=0; i<DS_THREAD_HEIGHT/4; ++i) 
+    {
+#pragma unroll
+        for (int j=0; j<DS_THREAD_WIDTH/8; ++j) 
+        {
+            sample32(sampler_out, 
+                 CM_BGR_ENABLE, //CM_BGR_ENABLE, CM_A_ENABLE
+                 src_idx,
+                 sampler_idx,
+                 x0 + (8*j*x_interval),
+                 y0 + (4*i*y_interval),
+                 x_interval,
+                 y_interval);
+
+            sampler_out += 128;
+
+            out_y.select<4,1,8,1>(i*4,j*8) = sampler_out.row(1).format<uchar>().select<32, 2>(1); // Y
+            tmp_u.select<4,1,8,1>(i*4,j*8) = sampler_out.row(2).format<uchar>().select<32, 2>(1); // U
+            tmp_v.select<4,1,8,1>(i*4,j*8) = sampler_out.row(0).format<uchar>().select<32, 2>(1); // V
+        }
+    }
+
+    write_plane(dst_idx, GENX_SURFACE_Y_PLANE, x, y, out_y);
+
+    out_uv.select<8, 1, 8, 2>(0, 0) = tmp_u.select<8, 2, 8, 2>(0, 0);
+    out_uv.select<8, 1, 8, 2>(0, 1) = tmp_v.select<8, 2, 8, 2>(0, 1);
+
+    write_plane(dst_idx, GENX_SURFACE_UV_PLANE, x, y/2, out_uv);
+}
+
+const short x_init[16] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+extern "C" _GENX_MAIN_ void
+downscale16(SurfaceIndex src_idx,
+    SamplerIndex sampler_idx,
+    SurfaceIndex dst_idx,
+    ushort image_width,
+    ushort image_height)
+{
+    ushort idx = get_thread_origin_x();
+    ushort idy = get_thread_origin_y();
+    ushort x = get_thread_origin_x() * DS_THREAD_WIDTH;
+    ushort y = get_thread_origin_y() * DS_THREAD_HEIGHT;
+
+    // calc interval
+    float x_interval = 1.0f / (image_width);
+    float y_interval = 1.0f / (image_height);
+
+    vector<short, 16> x_tmp(x_init);
+    vector<float, 16> x_pos;
+    vector<float, 16> y_pos;
+
+    x_pos = x_tmp + x;
+    x_pos *= x_interval;
+    y_pos = y * y_interval;
+
+    matrix<float, 3, DS_THREAD_WIDTH> sampler_out;
+    matrix<uchar, DS_THREAD_HEIGHT, DS_THREAD_WIDTH> out_y;
+    matrix<uchar, DS_THREAD_HEIGHT, DS_THREAD_WIDTH> out_u;
+    matrix<uchar, DS_THREAD_HEIGHT, DS_THREAD_WIDTH> out_v;
+    matrix<uchar, DS_THREAD_HEIGHT / 2, DS_THREAD_WIDTH> out_uv;
+
+#pragma unroll
+    for (int i = 0; i < DS_THREAD_HEIGHT; ++i)
+    {
+        sample16(sampler_out,
+            CM_BGR_ENABLE, //CM_BGR_ENABLE, CM_A_ENABLE
+            src_idx,
+            sampler_idx,
+            x_pos,
+            y_pos);
+
+        sampler_out = cm_mul<float>(sampler_out, 255.0f);
+
+        out_y.row(i) = sampler_out.row(1);
+        out_u.row(i) = sampler_out.row(2);
+        out_v.row(i) = sampler_out.row(0);
+
+        y_pos += y_interval;
+    }
+
+    write_plane(dst_idx, GENX_SURFACE_Y_PLANE, x, y, out_y);
+
+    out_uv.select<8, 1, 8, 2>(0, 0) = out_u.select<8, 2, 8, 2>(0, 0);
+    out_uv.select<8, 1, 8, 2>(0, 1) = out_v.select<8, 2, 8, 2>(0, 1);
+    write_plane(dst_idx, GENX_SURFACE_UV_PLANE, x, y/2, out_uv);
+}
